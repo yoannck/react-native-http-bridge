e95543ba69084c67aa1ae93141d9b7de18143bb3
Yoann Chane Kive <yoannck@gmail.com>
2019-08-25 11:59:20 +0900

    fix bug

--- httpServer.js
+++ httpServer.js
@@ -24,7 +24,7 @@ module.exports = {
 
     respond: function (requestId, code, type, body) {
         Server.respond(requestId, code, type, body);
-    }
+    },
 
     respondAudio: function (requestId, url) {
         Server.respondAudio(requestId, url);
